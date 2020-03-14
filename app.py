f1675daf7b9ae333c91b8134e6167fe208aa1450
Your Name <you@example.com>
2020-03-14 10:06:17 +0100

    added loop

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-music_data = pd.read_csv("music.csv")
-X = music_data.drop(columns=["genre"])
-y = music_data["genre"]
+while True:
+    music_data = pd.read_csv("music.csv")
+    X = music_data.drop(columns=["genre"])
+    y = music_data["genre"]
 
-model = DecisionTreeClassifier()
-model.fit(X , y)
+    model = DecisionTreeClassifier()
+    model.fit(X , y)
 
-age = input("What is your age? ")
-gender = input("What is your gender? ")
+    age = input("What is your age? ")
+    gender = input("What is your gender? ")
 
-if gender == "Male":
-    gender = 1
-elif gender == "Female":
-    gender = 0
+    if gender == "Male":
+        gender = 1
+    elif gender == "Female":
+        gender = 0
 
-prediction = model.predict([[age,gender]])[0]
-print("We recommend " + prediction)
+    prediction = model.predict([[age,gender]])[0]
+    print("We recommend " + prediction)
